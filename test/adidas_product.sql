7b83ea6ddd432a8c6ac69d899143f6f89668ff13
Anushka0708 <anushkasharma0809@gmail.com>
2019-10-02 22:05:15 +0530

    Change in the SQL file

--- test/adidas_product.sql
+++ test/adidas_product.sql
@@ -1,28 +1,9 @@
--- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
---
--- Host: localhost    Database: adidas
--- ------------------------------------------------------
--- Server version	8.0.17
+-- Dumping database structure for adidas
+CREATE DATABASE IF NOT EXISTS `adidas` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `adidas`;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `product`
---
-
-DROP TABLE IF EXISTS `product`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `product` (
+-- Dumping structure for table adidas.product
+CREATE TABLE IF NOT EXISTS `product` (
   `id` varchar(50) NOT NULL DEFAULT '',
   `name` varchar(50) NOT NULL DEFAULT '',
   `model_number` varchar(200) NOT NULL DEFAULT '',
@@ -40,25 +21,6 @@ CREATE TABLE `product` (
   `keywords` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `product`
---
-
-LOCK TABLES `product` WRITE;
-/*!40000 ALTER TABLE `product` DISABLE KEYS */;
-INSERT INTO `product` VALUES ('ABC001','New Gym Shoes','ABC','inline',119.95,99.96,119.95,NULL,NULL,'Subtitle001','Text001','UK Adidas shoes','Just a sample description','/shoes/gym.html','GYM | SHOES');
-/*!40000 ALTER TABLE `product` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-10-01 21:43:57
+-- Dumping data for table adidas.product:
+INSERT INTO `product` (`id`, `name`, `model_number`, `product_type`, `standard_price`, `standard_price_no_vat`, `currentPrice`, `meta_title`, `description_title`, `subtitle`, `text`, `site_name`, `description`, `canonical`, `keywords`) VALUES('HGTT199', 'Nite Jogger Shoes', 'BTO93', 'inline', 119.95, 99.96, 119.95, 'adidas Nite Jogger Shoes - Black | adidas UK', 'hey there', 'Modern cushioning updates this flashy \'80s standout.', 'Inspired by the 1980 Nite Jogger, these shoes shine bright with retro style and ', 'adidas United Kingdom', 'Shop for Nite Jogger Shoes - Black at adidas.co.uk! See all the styles ', 'Nite Jogger Shoes', '//www.adidas.co.uk/nite-jogger-shoes/CG7088.html');
