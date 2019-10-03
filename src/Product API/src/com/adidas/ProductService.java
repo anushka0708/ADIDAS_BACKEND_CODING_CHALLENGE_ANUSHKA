2115a50934fd91a98d4cfa7d25aa3725773577f0
Anushka0708 <anushkasharma0809@gmail.com>
2019-10-04 02:01:08 +0530

    Changes in documentation

--- src/Product API/src/com/adidas/ProductService.java
+++ src/Product API/src/com/adidas/ProductService.java
@@ -16,17 +16,41 @@ public class ProductService{
 			ResultSet rs=stmt.executeQuery("select * from product where id = '"+productID+"'");
 
 			while(rs.next()) {
-				productObject.setId(rs.getString("id"));
-				productObject.setName(rs.getString("name"));
-				productObject.setModel_number(rs.getString("model_number"));
-				productObject.setProduct_type(rs.getString("product_type"));
+productObject.setId(rs.getString("id"));
+				
+				if(rs.getString("name")==null)
+					productObject.setName("NA");
+				else productObject.setName(rs.getString("name"));
+				
+				if(rs.getString("model_number")==null)
+					productObject.setModel_number("NA");
+				else productObject.setModel_number(rs.getString("model_number"));
+				
+				if(rs.getString("product_type")==null)
+					productObject.setProduct_type("NA");
+				else productObject.setProduct_type(rs.getString("product_type"));
 
 				HashMap<String,String> metaData  = new HashMap<String,String>();
-				metaData.put("page_title", rs.getString("page_title"));
-				metaData.put("site_name",rs.getString("site_name"));
-				metaData.put("description",rs.getString("description"));
-				metaData.put("keywords",rs.getString("keywords"));
-				metaData.put("canonical",rs.getString("canonical"));
+				if(rs.getString("page_title")==null)
+					metaData.put("page_title", "NA");
+				else metaData.put("page_title", rs.getString("page_title"));
+				
+				if(rs.getString("site_name")==null)
+					metaData.put("site_name","NA");
+				else metaData.put("site_name",rs.getString("site_name"));
+				
+				if(rs.getString("description")==null)
+					metaData.put("description","NA");
+				else metaData.put("description",rs.getString("description"));
+				
+				if(rs.getString("keywords")==null)
+					metaData.put("keywords","NA");
+				else metaData.put("keywords",rs.getString("keywords"));
+				
+				if(rs.getString("canonical")==null)
+					metaData.put("canonical","NA");
+				else metaData.put("canonical",rs.getString("canonical"));
+				
 				productObject.setMeta(metaData);
 
 				HashMap<String,Double> pricingInformation  = new HashMap<String,Double>();
@@ -36,9 +60,18 @@ public class ProductService{
 				productObject.setPricing_information(pricingInformation);
 
 				HashMap<String, String> description = new HashMap<String, String>();
-				description.put("title",rs.getString("title"));
-				description.put("subtitle",rs.getString("subtitle"));
-				description.put("text",rs.getString("text"));
+				if(rs.getString("title")==null)
+					description.put("title","NA");
+				else description.put("title",rs.getString("title"));
+				
+				if(rs.getString("subtitle")==null)
+					description.put("subtitle","NA");
+				else description.put("subtitle",rs.getString("subtitle"));
+				
+				if(rs.getString("text")==null)
+					description.put("text","NA");
+				else description.put("text",rs.getString("text"));
+				
 				productObject.setProduct_description(description);
 				return productObject;
 			}
@@ -63,16 +96,40 @@ public class ProductService{
 				Product productObject = new Product();
 
 				productObject.setId(rs.getString("id"));
-				productObject.setName(rs.getString("name"));
-				productObject.setModel_number(rs.getString("model_number"));
-				productObject.setProduct_type(rs.getString("product_type"));
+				
+				if(rs.getString("name")==null)
+					productObject.setName("NA");
+				else productObject.setName(rs.getString("name"));
+				
+				if(rs.getString("model_number")==null)
+					productObject.setModel_number("NA");
+				else productObject.setModel_number(rs.getString("model_number"));
+				
+				if(rs.getString("product_type")==null)
+					productObject.setProduct_type("NA");
+				else productObject.setProduct_type(rs.getString("product_type"));
 
 				HashMap<String,String> metaData  = new HashMap<String,String>();
-				metaData.put("page_title", rs.getString("page_title"));
-				metaData.put("site_name",rs.getString("site_name"));
-				metaData.put("description",rs.getString("description"));
-				metaData.put("keywords",rs.getString("keywords"));
-				metaData.put("canonical",rs.getString("canonical"));
+				if(rs.getString("page_title")==null)
+					metaData.put("page_title", "NA");
+				else metaData.put("page_title", rs.getString("page_title"));
+				
+				if(rs.getString("site_name")==null)
+					metaData.put("site_name","NA");
+				else metaData.put("site_name",rs.getString("site_name"));
+				
+				if(rs.getString("description")==null)
+					metaData.put("description","NA");
+				else metaData.put("description",rs.getString("description"));
+				
+				if(rs.getString("keywords")==null)
+					metaData.put("keywords","NA");
+				else metaData.put("keywords",rs.getString("keywords"));
+				
+				if(rs.getString("canonical")==null)
+					metaData.put("canonical","NA");
+				else metaData.put("canonical",rs.getString("canonical"));
+				
 				productObject.setMeta(metaData);
 
 				HashMap<String,Double> pricingInformation  = new HashMap<String,Double>();
@@ -82,9 +139,18 @@ public class ProductService{
 				productObject.setPricing_information(pricingInformation);
 
 				HashMap<String, String> description = new HashMap<String, String>();
-				description.put("title",rs.getString("title"));
-				description.put("subtitle",rs.getString("subtitle"));
-				description.put("text",rs.getString("text"));
+				if(rs.getString("title")==null)
+					description.put("title","NA");
+				else description.put("title",rs.getString("title"));
+				
+				if(rs.getString("subtitle")==null)
+					description.put("subtitle","NA");
+				else description.put("subtitle",rs.getString("subtitle"));
+				
+				if(rs.getString("text")==null)
+					description.put("text","NA");
+				else description.put("text",rs.getString("text"));
+				
 				productObject.setProduct_description(description);
 
 				products.add(productObject);
